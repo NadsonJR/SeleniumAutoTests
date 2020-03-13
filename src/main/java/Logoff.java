d10bc201c59f7075421d9548eb7b5dd924b0692d
Diany Souza Andrade <dsouzaan@everis.com>
2020-03-13 15:16:06 -0300

    funcao logoff concluida

--- src/main/java/Logoff.java
+++ src/main/java/Logoff.java
@@ -3,11 +3,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-public class Login {
+public class Logoff {
+	
 	@Test
-	public void CT001_teste() throws Exception {
-
+	public void CT062() throws Exception {
+		
 		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
 		TakeScreenShotFunction screenShotFunction = new TakeScreenShotFunction();
 		WebDriver driver = new ChromeDriver();
@@ -33,12 +35,8 @@ public class Login {
 		WebElement password = driver.findElement(By.id("senha_lp"));
 
 		email.sendKeys("nadsonjunior@live.com");
-		System.out.println("Email preenchido");
-
 		password.sendKeys("Varusadc1");
-		System.out.println("Password Preenchido");
 
-		// Funtion Take a Screenshot
 		screenShotFunction.takeSaveScreenShot(count, className);
 		count++;
 		Thread.sleep(1500);
@@ -47,12 +45,26 @@ public class Login {
 		btnSubmitLogin.click();
 		
 		Thread.sleep(1500);
-		// Funtion Take a Screenshot
 		screenShotFunction.takeSaveScreenShot(count, className);
 		count++;
 		Thread.sleep(1500);
+		
+		Actions acao = new Actions(driver);
+		WebElement menuMouseOver = driver.findElement(By.linkText("Antonio"));
+		acao.moveToElement(menuMouseOver).build().perform();
+		
+		screenShotFunction.takeSaveScreenShot(count, className);
+		Thread.sleep(1500);
+		count++;
+		
+		driver.findElement(By.linkText("SAIR")).click();;
+		
+		screenShotFunction.takeSaveScreenShot(count, className);
+		Thread.sleep(1500);
+		count++;
 
-		driver.quit();
-
+		driver.quit();		
 	}
+	
+
 }
--- src/main/java/Pesquisa.java
+++ src/main/java/Pesquisa.java
@@ -48,4 +48,44 @@ public class Pesquisa {
 		driver.quit();
 	}
 	
+	@Test
+	public void CT044() throws Exception
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		TakeScreenShotFunction screenShotFunction = new TakeScreenShotFunction();
+		WebDriver driver = new ChromeDriver();
+		
+		String url = "https://www.floresonline.com.br/";
+		driver.manage().window().maximize();
+		driver.get(url);
+		
+		String className = Thread.currentThread().getStackTrace()[1].getMethodName();
+		int count = 0;
+		//tirando print da tela ativa
+		screenShotFunction.takeSaveScreenShot(count, className);
+		Thread.sleep(1500);
+		count++;
+		
+		Actions acao = new Actions(driver);
+		WebElement menuMouseOver = driver.findElement(By.id("custom-real-mainmenu-item-7"));
+		acao.moveToElement(menuMouseOver).build().perform();
+		
+		screenShotFunction.takeSaveScreenShot(count, className);
+		Thread.sleep(1500);
+		count++;
+		
+		driver.findElement(By.xpath("//div[@id='custom-real-mainmenu-inside-item-99']/div[2]")).click();
+		
+		screenShotFunction.takeSaveScreenShot(count, className);
+		Thread.sleep(1500);
+		count++;
+		
+		acao.sendKeys(Keys.PAGE_DOWN).perform();
+		screenShotFunction.takeSaveScreenShot(count, className);
+		Thread.sleep(1500);
+		count++;
+		
+		driver.quit();
+	}
+	
 }
